99717514229933a4a7ec6218bb05d156888b53ef
Varun Barad <varun@varunbarad.com>
2020-01-25 15:38:32 +0530

    Add presenter code to handle "New Note" button click

--- app/src/main/java/com/varunbarad/takehome/notes/screens/list_notes/ListNotesPresenter.kt
+++ app/src/main/java/com/varunbarad/takehome/notes/screens/list_notes/ListNotesPresenter.kt
@@ -1,13 +1,24 @@
 package com.varunbarad.takehome.notes.screens.list_notes
 
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.subscribeBy
+
 class ListNotesPresenter(
     private val view: ListNotesView
 ) {
-    fun onStart() {
+    private val serviceDisposables = CompositeDisposable()
 
+    fun onStart() {
+        this.serviceDisposables.add(
+            this.view
+                .onButtonNewNoteClick()
+                .subscribeBy {
+                    this.view.openNewNoteScreen()
+                }
+        )
     }
 
     fun onStop() {
-
+        this.serviceDisposables.clear()
     }
 }
