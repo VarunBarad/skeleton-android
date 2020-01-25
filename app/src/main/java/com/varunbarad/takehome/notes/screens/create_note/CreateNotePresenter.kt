a93e43c041b5889b8a24fcc815539b95c1fea0f9
Varun Barad <varun@varunbarad.com>
2020-01-25 19:48:46 +0530

    Add presenter code for Create Note screen

--- app/src/main/java/com/varunbarad/takehome/notes/screens/create_note/CreateNotePresenter.kt
+++ app/src/main/java/com/varunbarad/takehome/notes/screens/create_note/CreateNotePresenter.kt
@@ -1,19 +1,120 @@
 package com.varunbarad.takehome.notes.screens.create_note
 
+import android.util.Log
+import com.varunbarad.takehome.notes.external_services.local_database.model.DbNote
 import com.varunbarad.takehome.notes.repositories.NotesRepository
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.subscribeBy
 
 class CreateNotePresenter(
     private val view: CreateNoteView,
     private val notesRepository: NotesRepository
 ) {
+    companion object {
+        const val ERROR_MESSAGE_TITLE_BLANK = "Title cannot be blank"
+        const val ERROR_MESSAGE_CONTENT_BLANK = "Content cannot be blank"
+        const val ERROR_MESSAGE_TITLE_TOO_LONG = "Title cannot be longer than 100 characters"
+    }
+
     private val serviceDisposables = CompositeDisposable()
 
     fun onStart() {
-
+        this.serviceDisposables.add(
+            this.view
+                .onButtonSaveNoteClick()
+                .subscribeBy(
+                    onError = {}, // This is just placeholder as without this it throws some error
+                    onNext = { this.handleSaveButtonClick() }
+                )
+        )
     }
 
     fun onStop() {
         this.serviceDisposables.clear()
+    }
+
+    private fun handleSaveButtonClick() {
+        val titleValue = this.view.getTitleEditTextValue().trim()
+        val contentValue = this.view.getContentsEditTextValue().trim()
+
+        if (titleValue.isBlank()) {
+            if (contentValue.isBlank()) {
+                this.view.updateScreen(
+                    CreateNoteViewState(
+                        titleValue = titleValue,
+                        contentsValue = contentValue,
+                        titleErrorText = ERROR_MESSAGE_TITLE_BLANK,
+                        contentsErrorText = ERROR_MESSAGE_CONTENT_BLANK,
+                        showTitleError = true,
+                        showContentsError = true
+                    )
+                )
+            } else {
+                this.view.updateScreen(
+                    CreateNoteViewState(
+                        titleValue = titleValue,
+                        contentsValue = contentValue,
+                        titleErrorText = ERROR_MESSAGE_TITLE_BLANK,
+                        contentsErrorText = "",
+                        showTitleError = true,
+                        showContentsError = false
+                    )
+                )
+            }
+        } else if (titleValue.length > 100) {
+            if (contentValue.isBlank()) {
+                this.view.updateScreen(
+                    CreateNoteViewState(
+                        titleValue = titleValue,
+                        contentsValue = contentValue,
+                        titleErrorText = ERROR_MESSAGE_TITLE_TOO_LONG,
+                        contentsErrorText = ERROR_MESSAGE_CONTENT_BLANK,
+                        showTitleError = true,
+                        showContentsError = true
+                    )
+                )
+            } else {
+                this.view.updateScreen(
+                    CreateNoteViewState(
+                        titleValue = titleValue,
+                        contentsValue = contentValue,
+                        titleErrorText = ERROR_MESSAGE_TITLE_TOO_LONG,
+                        contentsErrorText = "",
+                        showTitleError = true,
+                        showContentsError = false
+                    )
+                )
+            }
+        } else if (contentValue.isBlank()) {
+            this.view.updateScreen(
+                CreateNoteViewState(
+                    titleValue = titleValue,
+                    contentsValue = contentValue,
+                    titleErrorText = "",
+                    contentsErrorText = ERROR_MESSAGE_CONTENT_BLANK,
+                    showTitleError = false,
+                    showContentsError = true
+                )
+            )
+        } else {
+            this.serviceDisposables.add(
+                this.notesRepository
+                    .insertNewNote(
+                        DbNote(
+                            title = titleValue,
+                            contents = contentValue
+                        )
+                    ).subscribeBy(
+                        onError = {
+                            // ToDo: Show message on screen
+                            Log.e("varunvarun", it.message ?: "varun error message")
+                        },
+                        onSuccess = { noteId ->
+                            // ToDo: Pass note-id here
+                            this.view.openNoteDetailsScreen()
+                        }
+                    )
+            )
+        }
     }
 }
