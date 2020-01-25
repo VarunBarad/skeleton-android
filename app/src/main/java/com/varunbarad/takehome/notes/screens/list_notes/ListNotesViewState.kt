9f19f1e9769abca66019184b4adb5e63b3315cbf
Varun Barad <varun@varunbarad.com>
2020-01-26 02:32:29 +0530

    Add flag for visibility of no-stored-notes message

--- app/src/main/java/com/varunbarad/takehome/notes/screens/list_notes/ListNotesViewState.kt
+++ app/src/main/java/com/varunbarad/takehome/notes/screens/list_notes/ListNotesViewState.kt
@@ -3,5 +3,6 @@ package com.varunbarad.takehome.notes.screens.list_notes
 import com.varunbarad.takehome.notes.model.UiNote
 
 data class ListNotesViewState(
-    val notes: List<UiNote>
+    val notes: List<UiNote>,
+    val isNoStoredNotesMessageVisible: Boolean
 )
