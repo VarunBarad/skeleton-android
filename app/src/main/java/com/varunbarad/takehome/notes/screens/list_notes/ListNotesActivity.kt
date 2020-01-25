e3c47fd62df3393970b95647bb520a43f87b53cc
Varun Barad <varun@varunbarad.com>
2020-01-25 15:35:03 +0530

    Add skeleton of List Notes presenter and integrate it into activity

--- app/src/main/java/com/varunbarad/takehome/notes/screens/list_notes/ListNotesActivity.kt
+++ app/src/main/java/com/varunbarad/takehome/notes/screens/list_notes/ListNotesActivity.kt
@@ -6,21 +6,45 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.varunbarad.takehome.notes.R
 import com.varunbarad.takehome.notes.databinding.ActivityListNotesBinding
+import com.varunbarad.takehome.notes.util.Event
+import io.reactivex.Observable
 
-class ListNotesActivity : AppCompatActivity() {
+class ListNotesActivity : AppCompatActivity(), ListNotesView {
     private lateinit var viewBinding: ActivityListNotesBinding
+
+    private lateinit var presenter: ListNotesPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.viewBinding = DataBindingUtil.setContentView(this, R.layout.activity_list_notes)
         this.setSupportActionBar(this.viewBinding.toolbar)
 
-        this.viewBinding.buttonNewNote.setOnClickListener {
-            Toast.makeText(
-                this,
-                "Open screen to create new note",
-                Toast.LENGTH_SHORT
-            ).show()
+        this.presenter = ListNotesPresenter(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        this.presenter.onStart()
+    }
+
+    override fun onStop() {
+        this.presenter.onStop()
+
+        this.viewBinding.buttonNewNote.setOnClickListener(null)
+
+        super.onStop()
+    }
+
+    override fun onButtonNewNoteClick(): Observable<Event> {
+        return Observable.create { emitter ->
+            this.viewBinding.buttonNewNote.setOnClickListener { emitter.onNext(Event.IGNORE) }
+            emitter.setCancellable { this.viewBinding.buttonNewNote.setOnClickListener(null) }
         }
+    }
+
+    override fun openNewNoteScreen() {
+        // ToDo: Open New Note screen
+        Toast.makeText(this, "Open new note screen", Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/varunbarad/takehome/notes/screens/list_notes/ListNotesPresenter.kt
@@ -0,0 +1,13 @@
+package com.varunbarad.takehome.notes.screens.list_notes
+
+class ListNotesPresenter(
+    private val view: ListNotesView
+) {
+    fun onStart() {
+
+    }
+
+    fun onStop() {
+
+    }
+}
