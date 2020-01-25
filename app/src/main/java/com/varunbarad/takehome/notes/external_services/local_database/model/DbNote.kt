949c9230de4638fa1d7d8f94fa58633bb381a7ac
Varun Barad <varun@varunbarad.com>
2020-01-25 19:09:09 +0530

    Make note-id long from int

--- app/src/main/java/com/varunbarad/takehome/notes/external_services/local_database/model/DbNote.kt
+++ app/src/main/java/com/varunbarad/takehome/notes/external_services/local_database/model/DbNote.kt
@@ -3,7 +3,7 @@ package com.varunbarad.takehome.notes.external_services.local_database.model
 import java.util.*
 
 data class DbNote(
-    val id: Int? = null,
+    val id: Long? = null,
     val title: String,
     val contents: String,
     val timestamp: Date? = null
