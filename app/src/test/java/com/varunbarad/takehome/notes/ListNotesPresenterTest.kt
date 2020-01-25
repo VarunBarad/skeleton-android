fffdb53fcf66ac50a2657b47c5630a3069ed7fe6
Varun Barad <varun@varunbarad.com>
2020-01-26 01:22:32 +0530

    Add unit tests for loading items from repository and showing on screen

--- app/src/test/java/com/varunbarad/takehome/notes/ListNotesPresenterTest.kt
+++ app/src/test/java/com/varunbarad/takehome/notes/ListNotesPresenterTest.kt
@@ -1,9 +1,13 @@
 package com.varunbarad.takehome.notes
 
+import com.varunbarad.takehome.notes.external_services.local_database.model.DbNote
+import com.varunbarad.takehome.notes.repositories.NotesRepository
 import com.varunbarad.takehome.notes.screens.list_notes.ListNotesPresenter
 import com.varunbarad.takehome.notes.screens.list_notes.ListNotesView
+import com.varunbarad.takehome.notes.screens.list_notes.ListNotesViewState
 import com.varunbarad.takehome.notes.util.Event
 import com.varunbarad.takehome.notes.util.ThreadSchedulers
+import com.varunbarad.takehome.notes.util.toUiNote
 import io.reactivex.Observable
 import org.junit.After
 import org.junit.Before
@@ -11,10 +15,13 @@ import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito.*
 import org.mockito.MockitoAnnotations
+import java.util.*
 
 class ListNotesPresenterTest {
     @Mock
     private lateinit var listNotesView: ListNotesView
+    @Mock
+    private lateinit var notesRepository: NotesRepository
 
     private lateinit var listNotesPresenter: ListNotesPresenter
 
@@ -23,7 +30,10 @@ class ListNotesPresenterTest {
         MockitoAnnotations.initMocks(this)
         ThreadSchedulers.enableTesting()
 
-        this.listNotesPresenter = ListNotesPresenter(this.listNotesView)
+        this.listNotesPresenter = ListNotesPresenter(
+            this.listNotesView,
+            this.notesRepository
+        )
     }
 
     @After
@@ -33,7 +43,9 @@ class ListNotesPresenterTest {
 
     @Test
     fun testNewNoteScreenOpenWhenNewNoteButtonClick() {
+        `when`(notesRepository.getAllNotesSortedReverseChronologically()).thenReturn(Observable.never())
         `when`(listNotesView.onButtonCreateNoteClick()).thenReturn(Observable.just(Event.IGNORE))
+        `when`(listNotesView.onNoteClick()).thenReturn(Observable.never())
 
         listNotesPresenter.onStart()
 
@@ -42,10 +54,49 @@ class ListNotesPresenterTest {
 
     @Test
     fun testNewNoteScreenNotOpenWhenNewNoteButtonNotClick() {
+        `when`(notesRepository.getAllNotesSortedReverseChronologically()).thenReturn(Observable.never())
         `when`(listNotesView.onButtonCreateNoteClick()).thenReturn(Observable.never())
+        `when`(listNotesView.onNoteClick()).thenReturn(Observable.never())
 
         listNotesPresenter.onStart()
 
         verify(listNotesView, never()).openCreateNoteScreen()
     }
+
+    @Test
+    fun testShowZeroItemsWhenZeroItemsReturnedFromRepository() {
+        `when`(notesRepository.getAllNotesSortedReverseChronologically()).thenReturn(
+            Observable.just(
+                emptyList()
+            )
+        )
+        `when`(listNotesView.onButtonCreateNoteClick()).thenReturn(Observable.never())
+        `when`(listNotesView.onNoteClick()).thenReturn(Observable.never())
+
+        listNotesPresenter.onStart()
+
+        verify(listNotesView).updateScreen(ListNotesViewState(emptyList()))
+    }
+
+    @Test
+    fun testShow4ItemsWhen4ItemsReturnedFromRepository() {
+        `when`(notesRepository.getAllNotesSortedReverseChronologically()).thenReturn(
+            Observable.just(
+                notes
+            )
+        )
+        `when`(listNotesView.onButtonCreateNoteClick()).thenReturn(Observable.never())
+        `when`(listNotesView.onNoteClick()).thenReturn(Observable.never())
+
+        listNotesPresenter.onStart()
+
+        verify(listNotesView).updateScreen(ListNotesViewState(notes.map { it.toUiNote() }))
+    }
+
+    private val notes: List<DbNote> = listOf(
+        DbNote(1, "Title 1", "Content 1", Date(System.currentTimeMillis())),
+        DbNote(2, "Title 2", "Content 2", Date(System.currentTimeMillis())),
+        DbNote(3, "Title 3", "Content 3", Date(System.currentTimeMillis())),
+        DbNote(4, "Title 4", "Content 4", Date(System.currentTimeMillis()))
+    ).reversed() // We reverse these to get reverse-chronological sorting here
 }
