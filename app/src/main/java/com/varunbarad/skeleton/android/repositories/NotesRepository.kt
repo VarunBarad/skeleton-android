445609d40c07ad313c264503a9d36d85bfc062ec
Varun Barad <varun@varunbarad.com>
2020-03-06 18:58:36 +0530

    Add method in repository to update note

--- app/src/main/java/com/varunbarad/skeleton/android/repositories/NotesRepository.kt
+++ app/src/main/java/com/varunbarad/skeleton/android/repositories/NotesRepository.kt
@@ -1,6 +1,7 @@
 package com.varunbarad.skeleton.android.repositories
 
 import com.varunbarad.skeleton.android.external_services.local_database.model.DbNote
+import io.reactivex.Completable
 import io.reactivex.Observable
 import io.reactivex.Single
 
@@ -11,4 +12,6 @@ interface NotesRepository {
     fun getBookmarkedNotesSortedReverseChronologically(): Observable<List<DbNote>>
 
     fun getNoteDetails(noteId: Long): Single<DbNote>
+
+    fun updateNote(note: DbNote): Completable
 }
--- app/src/main/java/com/varunbarad/skeleton/android/repositories/RoomNotesRepository.kt
+++ app/src/main/java/com/varunbarad/skeleton/android/repositories/RoomNotesRepository.kt
@@ -3,6 +3,7 @@ package com.varunbarad.skeleton.android.repositories
 import com.varunbarad.skeleton.android.external_services.local_database.NotesDao
 import com.varunbarad.skeleton.android.external_services.local_database.model.DbNote
 import com.varunbarad.skeleton.android.util.ThreadSchedulers
+import io.reactivex.Completable
 import io.reactivex.Observable
 import io.reactivex.Single
 import java.util.*
@@ -29,6 +30,12 @@ class RoomNotesRepository(private val notesDao: NotesDao) : NotesRepository {
 
     override fun getNoteDetails(noteId: Long): Single<DbNote> {
         return notesDao.getNoteDetails(noteId)
+            .subscribeOn(ThreadSchedulers.io())
+            .observeOn(ThreadSchedulers.main())
+    }
+
+    override fun updateNote(note: DbNote): Completable {
+        return notesDao.updateNoteDetails(note)
             .subscribeOn(ThreadSchedulers.io())
             .observeOn(ThreadSchedulers.main())
     }
